98b79bfcd0ee68e47f44f8c343bf9b2fb89720f0
uu29 <60836178+uu29@users.noreply.github.com>
2023-01-15 13:51:36 +0900

    feat: textarea rows cols 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Head from 'next/head';
 import styled from '@emotion/styled';
 import { pdf } from '@react-pdf/renderer';
@@ -9,14 +9,23 @@ import RowLine from '../components/textarea/RowLine';
 import TimerController from '../components/TimerController';
 
 export default function Home() {
+  const textRef = useRef<HTMLTextAreaElement>(null);
   const [text, setText] = useState('');
-  const rowsPerPage = 30;
-  const handleChange = (value: string) => setText(value);
   const saveAsPDF = async () => {
     const blob = await pdf((
       <PDFDocument content={text} />
     )).toBlob();
     saveAs(blob, '제13회_변호사_시험_답안지');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value, scrollHeight, clientHeight } = e.target;
+    setText(value);
+    if (scrollHeight > clientHeight) {
+      console.log('yes');
+    } else {
+      console.log('no');
+    }
   };
 
   return (
@@ -46,10 +55,13 @@ export default function Home() {
         </Title>
         <TextareaBlock>
           <Textarea
-            onChange={(e) => handleChange(e.target.value)}
+            ref={textRef}
+            onChange={handleChange}
             lineHeight={DEFAULT_LINE_HEIGHT}
+            rows={ROWS_PER_PAGE}
+            cols={COLS}
           />
-          {Array.from({ length: rowsPerPage }).map((_, index) => (
+          {Array.from({ length: ROWS_PER_PAGE }).map((_, index) => (
             <RowLine
               lineNumber={index + 1}
               key={index}
@@ -63,6 +75,8 @@ export default function Home() {
   );
 }
 
+const ROWS_PER_PAGE = 30;
+const COLS = 37 * 1.5;
 export const DEFAULT_LINE_HEIGHT = 31;
 
 const Background = styled.div`
@@ -79,6 +93,7 @@ const Main = styled.main`
   margin: 2.5rem auto;
   max-width: 1200px;
   background: #fff;
+  height: 1140px;
 `;
 
 const TextareaBlock = styled.div`
@@ -98,15 +113,13 @@ const Title = styled.h1`
 const Textarea = styled.textarea<{ lineHeight?: number }>`
   display: block;
   resize: none;
-  width: 100%;
-  max-width: 520px;
   margin: auto;
   padding-top: 3px;
-  min-height: 1000px;
   outline: 0;
   border: 0;
   background: transparent;
   line-height: ${({ lineHeight }) => (lineHeight ? `${lineHeight}px` : '31px')};
+  //overflow: hidden;
 `;
 
 const Controllers = styled.div`
